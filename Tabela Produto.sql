72cf559f28c4385a267f8f9051d45dba3ab187b4
ulyynuna2608 <101338527+ulyynuna2608@users.noreply.github.com>
2023-02-28 09:51:33 -0300

    Update Tabela Produto.sql

--- Tabela Produto.sql
+++ Tabela Produto.sql
@@ -1,3 +1,5 @@
+//Prova 14/03 (Tipos de dados SQL, Criar Tabela, Inserir)\\
+
 select * from produto1
 
 
@@ -22,3 +24,15 @@ insert into livro01 values('4556', 'Harrry Potter','J.K. Rowling', 'Ficção')
 insert into livro01 values('3445', 'Crônicas do gelo e fogo','George R.R. Martins', 'Acão e Aventura')
 insert into livro01 values('5667', 'Jogos Vorazes','Suzan Collins', 'Ficção')
 insert into livro01 values('7889', 'O fururo da humanidade','Augusto Cury', 'Ficção')
+
+select * from usuario01 
+
+insert into usuario01 values  ('Fausto Silva', 'fausto@hotmail.com', '121212')
+
+insert into usuario01 values ('Ivete Sangalo', 'Sangalo@hotmail.com', '151515')
+insert into usuario01 values ('Laudia Leite', 'leite@hotmail.com', '131313')
+insert into usuario01 values ('Toni Ramos', 'ramos@hotmail.com', '181818')
+insert into usuario01 values ('Rodrigo Faro', 'faroo@hotmail.com', '171717')
+insert into usuario01 values ('Danilo Gentile', 'gentile@hotmail.com', '252525')
+insert into usuario01 values ('Rafael Basto', 'basto@hotmail.com', '363636')
+insert into usuario01 values ('Emilio Surita', 'fausto@hotmail.com', '858585')
