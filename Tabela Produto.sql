e879307631a9f9e39278ff6cd90f47b9f96b6865
ulyynuna2608 <101338527+ulyynuna2608@users.noreply.github.com>
2023-02-28 09:04:53 -0300

    Update Tabela Produto.sql

--- Tabela Produto.sql
+++ Tabela Produto.sql
@@ -6,3 +6,10 @@ select * from produto1
   
   insert into produto1 values ('Borracha', 'Faber Castel', '0.50')
   insert into produto1 values ('Corretivo', 'Faber Castel', '8.99')
+  
+  create table livro01(
+isbn smallint not null primary key,
+titulo nvarchar(80) not null,
+autor nvarchar(60) not null,
+genro nvarchar (30) not null
+)
