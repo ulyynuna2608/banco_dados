0d4355ab0466db4ad5686832e00875a6bdd6e534
ulyynuna2608 <101338527+ulyynuna2608@users.noreply.github.com>
2023-02-28 08:54:23 -0300

    Add files via upload

---
+++ Tabela Produto.sql
@@ -0,0 +1,8 @@
+select * from produto1
+
+
+  insert into produto1 values ('caneta preta', 'Bic', '1.50')
+  insert into produto1 values ('caneta vermelha', 'Bic', '1.50')
+  
+  insert into produto1 values ('Borracha', 'Faber Castel', '0.50')
+  insert into produto1 values ('Corretivo', 'Faber Castel', '8.99')
