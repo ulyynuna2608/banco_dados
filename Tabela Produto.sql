22ef379401676d46f2292827fbc2d9a7f8a8a75b
ulyynuna2608 <101338527+ulyynuna2608@users.noreply.github.com>
2023-02-28 09:16:29 -0300

    Update Tabela Produto.sql

--- Tabela Produto.sql
+++ Tabela Produto.sql
@@ -13,3 +13,12 @@ titulo nvarchar(80) not null,
 autor nvarchar(60) not null,
 genro nvarchar (30) not null
 )
+
+select * from livro01
+
+insert into livro01 values('1223', 'Senhor dos Aneis','Tolkien', 'Aventura')
+insert into livro01 values('2334', 'Um sorriso ou dois','Frederico Elboni', 'Romance')
+insert into livro01 values('4556', 'Harrry Potter','J.K. Rowling', 'Ficção')
+insert into livro01 values('3445', 'Crônicas do gelo e fogo','George R.R. Martins', 'Acão e Aventura')
+insert into livro01 values('5667', 'Jogos Vorazes','Suzan Collins', 'Ficção')
+insert into livro01 values('7889', 'O fururo da humanidade','Augusto Cury', 'Ficção')
