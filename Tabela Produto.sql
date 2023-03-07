9dbe905bff214a7b8da9c93d32f33d8845b4cc11
ulyynuna2608 <101338527+ulyynuna2608@users.noreply.github.com>
2023-03-07 09:35:16 -0300

    Update Tabela Produto.sql

--- Tabela Produto.sql
+++ Tabela Produto.sql
@@ -36,3 +36,72 @@ insert into usuario01 values ('Rodrigo Faro', 'faroo@hotmail.com', '171717')
 insert into usuario01 values ('Danilo Gentile', 'gentile@hotmail.com', '252525')
 insert into usuario01 values ('Rafael Basto', 'basto@hotmail.com', '363636')
 insert into usuario01 values ('Emilio Surita', 'fausto@hotmail.com', '858585')
+
+create table Minecraft01(
+CRM int not null primary key,
+Nome nvarchar (10) not null,
+rua  nvarchar (10) not null,
+bairro  nvarchar (10) not nul,
+cidade  nvarchar (10) not null,
+especialidade nvarchar (10) not null
+)
+
+insert into medico01 values (
+'101112', 'Kate Perry', 'Tamoios 87', 'centro','Tupã', 'Ortopedista'
+)
+insert into medico01 values (
+'202122', 'David Backham', 'Aimores 11', 'centro','Tupã', 'Pediatra'
+)
+insert into medico01 values (
+'303132', 'Handersson Rodrigues', 'Tapuias 34', 'centro', 'Tupã','Otorrinologista'
+)
+insert into medico01 values (
+'404142', 'Cassio Reis', 'Hiroshima', 'centro', 'Tupã','Otalmologista'
+)
+insert into medico01 values (
+'505152', 'Evaline Rodrigues', 'Bororos 54', 'centro', 'Tupã','Neurologista'
+)
+insert into medico01 values (
+'606162', 'Diana Franco', 'Botocudos 34', 'centro', 'Tupã','Cardiologista'
+)
+
+
+
+create table Minecraft01(
+codigo int not null primary key identity,
+Mob nvarchar (10) not null,
+Hostil nvarchar (10) not null,
+Vidas int not null,
+Mundo nvarchar (10) not null,
+Inventario nvarchar (10) not null
+)
+
+select * from Minecraft01
+insert into Minecraft01 values (
+'Creeper', 'Sim', '20', 'Overwold','Não'
+)
+
+insert into Minecraft01 values (
+'Ovelha', 'Não', '10', 'Nether','Não'
+)
+
+insert into Minecraft01 values (
+'Ghast', 'Sim', '20', 'Overwold','Não'
+)
+
+insert into Minecraft01 values (
+'Steve', 'Sim', '20', 'Todos','Sim'
+)
+
+insert into Minecraft01 values (
+'Enderman', 'Neutro', '40', 'Overwold','Não'
+)
+
+insert into Minecraft01 values (
+'Herobrine', 'Sim', '500', 'Todos','Não'
+)
+
+insert into Minecraft01 values (
+'Esqueleto', 'Sim', '20', 'Overwold','Não'
+)
+
